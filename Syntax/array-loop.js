8fda9b18ff75d40e36976e66f3db61087bd87240
Lee Jun Young <ljy930521@naver.com>
2020-03-26 10:10:49 +0900

    app organize

---
+++ Syntax/array-loop.js
@@ -0,0 +1,11 @@
+var number = [1, 233, 3234, 132, 6];
+
+var i =0;
+
+var total = 0;
+while(i<number.length){
+    total = total + number[i] 
+    
+    i = i + 1;
+}
+console.log(`total : ${total}`);
---
+++ Syntax/function.js
@@ -0,0 +1,20 @@
+import { setupMaster } from "cluster";
+
+f123();
+
+
+function f123(){
+    console.log(1);
+    console.log(2);
+    console.log(3);
+}
+
+console.log(Math.round(1.2));
+console.log(Math.round(1.6));
+
+function sum(first, second){ // parameter 매개변수
+   return first+second;
+}
+
+sum(2,4);// argument 인자
+
