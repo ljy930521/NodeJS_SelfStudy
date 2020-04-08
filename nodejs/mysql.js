269f745b2a5f57739549b463dfdbf4480d83fab6
Lee Jun Young <ljy930521@naver.com>
2020-04-09 00:24:29 +0900

    nodejs+MySQL

---
+++ nodejs/mysql.js
@@ -0,0 +1,18 @@
+var mysql      = require('mysql');
+var connection = mysql.createConnection({
+  host     : 'localhost',
+  user     : 'root',
+  password : '122312',
+  database : 'opentutorials'
+});
+ 
+connection.connect();
+ 
+connection.query('SELECT * FROM topic', function (error, results, fields) {
+  if (error) {//throw error;
+    console.log(error);
+  }  
+  console.log(results);
+});
+ 
+connection.end();
---
+++ password.js
@@ -0,0 +1,4 @@
+module.exports = {
+    id:'juning',
+    password:'1111'
+}
