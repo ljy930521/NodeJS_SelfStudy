849c6ad092e2a9b7e9afdc7e18ede234a27333d7
Lee Jun Young <ljy930521@naver.com>
2020-04-27 02:50:48 +0900

    author_update,delete

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 var http = require('http');
 var url = require('url');
 var topic = require('./lib/topic');
-var author = require('./lib/author.js');
+var author = require('./lib/author');
 
 var app = http.createServer(function (request, response) {
   var _url = request.url;
@@ -25,6 +25,14 @@ var app = http.createServer(function (request, response) {
     topic.delete_process(request,response);
   } else if (pathname === '/author') {
     author.home(request,response);
+  } else if (pathname === '/author/create_process'){
+    author.create_process(request,response);
+  } else if (pathname === '/author/update') {
+    author.update(request,response);
+  } else if (pathname === '/author/update_process') {
+    author.update_process(request,response);
+  } else if (pathname === '/author/delete_process') {
+    author.delete_process(request,response);  
   } else {
     response.writeHead(404);
     response.end('Not found');
